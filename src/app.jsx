bcc2baa3e81da573ebd96c7ce513007f02920afb
dermiie <demioyeniyi@gmail.com>
2024-10-27 11:01:02 +0000

    Created action functions in creating new orders and making use for reactRouter Form component

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,9 @@ import Home from './ui/Home';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Order, { loader as orderLoader } from './features/order/Order';
 import Cart from './features/cart/Cart';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 import AppLayout from './ui/AppLayout';
 import Error from './ui/Error';
 
@@ -20,7 +22,11 @@ const routes = createBrowserRouter([
         errorElement: <Error></Error>,
       },
       { path: 'cart', element: <Cart></Cart> },
-      { path: 'order/new', element: <CreateOrder></CreateOrder> },
+      {
+        path: 'order/new',
+        element: <CreateOrder></CreateOrder>,
+        action: createOrderAction,
+      },
       {
         path: 'order/:orderId',
         element: <Order></Order>,
