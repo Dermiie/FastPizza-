805bfa7c12ecd217b4eeba46a383d9051275fa75
dermiie <demioyeniyi@gmail.com>
2025-01-05 10:32:54 +0000

    Created the cart slice and added more redux functions in handling the cart slice

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+  cart: []
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    //new item object
+    addItem(state, action) {
+      state.cart.push(action.payload)
+    },
+
+    deleteItem(state, action) {
+      state.cart.filter((item) => item.pizzaId !== action.payload)
+    },
+
+    increaseItemQuantity(state, action){
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+
+    decreaseItemQuantity(state, action){
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+
+    },
+
+    clearCart(state){
+      state.cart = []
+    }
+  }
+
+});
+
+export const {addItem, deleteItem, decreaseItemQuantity, increaseItemQuantity} = cartSlice.actions
+
+export default cartSlice.reducer
